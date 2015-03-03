e6de59c4d5078fc2de07c40c18a1c7c8e4ce571e
elecpiano <elecpiano@gmail.com>
2015-03-03 15:32:59 +0800

    image button

---
+++ MeiTuTieTie/MeiTuTieTie/MeiTuTieTie.Shared/Control/ContentButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Shared.Control
+{
+    public class ContentButton : ContentControl
+    {
+        protected override void OnPointerPressed(Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+            VisualStateManager.GoToState(this, "Pressed", false);
+        }
+
+        protected override void OnPointerExited(Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            base.OnPointerExited(e);
+            VisualStateManager.GoToState(this, "Normal", true);
+        }
+
+        protected override void OnPointerReleased(Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+            VisualStateManager.GoToState(this, "Normal", true);
+        }
+
+    }
+}
---
+++ MeiTuTieTie/MeiTuTieTie/MeiTuTieTie.Shared/Control/ImageButton.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Shared.Control
+{
+    public class ImageButton : ContentControl
+    {
+        #region Property
+
+        public ImageSource NormalImage
+        {
+            get { return (ImageSource)GetValue(NormalImageProperty); }
+            set
+            {
+                SetValue(NormalImageProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty NormalImageProperty =
+            DependencyProperty.Register("NormalImage", typeof(ImageSource), typeof(ImageSwitch), new PropertyMetadata(null));
+
+        public ImageSource PressedImage
+        {
+            get { return (ImageSource)GetValue(PressedImageProperty); }
+            set
+            {
+                SetValue(PressedImageProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty PressedImageProperty =
+            DependencyProperty.Register("PressedImage", typeof(ImageSource), typeof(ImageSwitch), new PropertyMetadata(null));
+
+        #endregion
+
+        protected override void OnPointerPressed(Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+            VisualStateManager.GoToState(this, "Pressed", false);
+        }
+
+        protected override void OnPointerExited(Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            base.OnPointerExited(e);
+            VisualStateManager.GoToState(this, "Normal", true);
+        }
+
+        protected override void OnPointerReleased(Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+            VisualStateManager.GoToState(this, "Normal", true);
+        }
+
+    }
+}
